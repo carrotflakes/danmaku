b111030d56e08e81968e9ac7dffd77cf63545ba6
carrotflakes <carrotflakes@gmail.com>
2019-05-27 23:49:27 +0900

    remove sequence.js

--- src/scene/game.js
+++ src/scene/game.js
@@ -1,9 +1,12 @@
 import {Scene} from './scene';
 import {global} from '../global';
 import {Player} from '../player';
-import {Enemy} from '../enemy';
-import {codeGen} from '../sequence';
 import {BGM} from '../bgm';
+import {Enemy} from '../enemy';
+import {Enemy2} from '../enemy2';
+import {Enemy3} from '../enemy3';
+import {Enemy4} from '../enemy4';
+import {wait, sleep, fork, join} from '../vm';
 
 export class Game extends Scene {
   constructor(exit) {
@@ -54,4 +57,43 @@ export class Game extends Scene {
     ctx.font = '18px monospace';
     ctx.fillText('score: ' + global.score, 20, 20);
   }
+}
+
+export function* codeGen() {
+  const {spawn, width, height} = global;
+  while (true) {
+    yield waitForNoEnemy();
+    yield sleep(0.1);
+    spawn(new Enemy4({
+      x: Math.random() * width, y: -10,
+    }));
+    yield waitForNoEnemy();
+    yield sleep(0.1);
+    spawn(new Enemy({
+      x: width / 2, y: -10,
+      targetX: width / 3, targetY: height / 3
+    }));
+    yield waitForNoEnemy();
+    yield sleep(0.1);
+    spawn(new Enemy2({
+      x: width / 2, y: -10,
+      targetX: width * 2 / 3, targetY: height / 3
+    }));
+    yield waitForNoEnemy();
+    yield sleep(0.1);
+    const x = Math.random() * width;
+    for (let i = 0; i < 5; i++) {
+      spawn(new Enemy3({
+        x: x, y: -10,
+      }));
+      yield sleep(0.1);
+    }
+  }
+}
+
+export function* waitForNoEnemy() {
+  const {entities} = global;
+  yield wait();
+  while (entities.find(e => e instanceof Enemy))
+    yield wait();
 }
